1d233d04bcf1d7d470e68c4eefa9d5e0b2b9acb5
Jose Barrameda <jagbarrameda@gmail.com>
2020-06-17 16:42:56 -0400

    Added a ddb table

--- src/CrudApi/CrudApiStack.cs
+++ src/CrudApi/CrudApiStack.cs
@@ -1,12 +1,39 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.DynamoDB;
 
 namespace CrudApi
 {
+    
     public class CrudApiStack : Stack
     {
         internal CrudApiStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // The code that defines your stack goes here
+            CreateResources(scope);
+        }
+
+        private void CreateResources(Construct scope)
+        {
+            CreateDdb(this);
+        }
+
+        /**
+         * Creates the ddb storage of the data
+         */
+        private void CreateDdb(Construct scope)
+        {
+            string tableId = "crudTable";
+            TableProps props = new TableProps
+            {
+                PartitionKey = new Attribute
+                {
+                    Name = "partitionKey",
+                    Type = AttributeType.STRING
+                },
+                BillingMode = BillingMode.PAY_PER_REQUEST,
+                RemovalPolicy = RemovalPolicy.DESTROY
+            };
+            Table t = new Table(scope, tableId, props);
         }
     }
 }
