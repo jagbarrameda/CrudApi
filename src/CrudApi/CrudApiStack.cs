898186c53d12e172ca979dc9cb801254295ecab0
Jose Barrameda <jagbarrameda@gmail.com>
2020-06-17 15:58:07 -0400

    Initial commit

---
+++ src/CrudApi/CrudApiStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CrudApi
+{
+    public class CrudApiStack : Stack
+    {
+        internal CrudApiStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
